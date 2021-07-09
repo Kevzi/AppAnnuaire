35b4b6f49e5c5584f8cb145ce60b788a31a0ac44
Kevzi <kev92260@gmail.com>
2021-07-09 13:50:41 +0200

    Add files via upload

---
+++ DllLibrary/Contact.cs
@@ -0,0 +1,118 @@
+﻿namespace DllLibrary
+{
+    /// <summary>
+    ///     La classe abstraite Contact permettant de générer les contacts.
+    /// </summary>
+    public abstract class Contact
+    {
+        /// <summary>
+        ///     Le string qui récupère le nom.
+        /// </summary>
+        private string nom;
+
+        /// <summary>
+        ///     Le string qui récupère le prénom.
+        /// </summary>
+        private string prenom;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Contact" /> class.
+        /// </summary>
+        /// <param name="id">
+        ///     l'id.
+        /// </param>
+        /// <param name="n">
+        ///     Le nom.
+        /// </param>
+        /// <param name="p">
+        ///     Le prénom.
+        /// </param>
+        /// <param name="t">
+        ///     Le téléphone.
+        /// </param>
+        /// <param name="c">
+        ///     Le type de contact.
+        /// </param>
+        protected Contact(int id, string n, string p, string t, string c)
+        {
+            Nom = n;
+            Prenom = p;
+            Telephone = t;
+            TypeContact = c;
+            Id = id;
+        }
+
+        /// <summary>
+        ///     Gets or sets the id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the nom.
+        /// </summary>
+        public string Nom
+        {
+            get
+            {
+                // Si la taille du nom est supérieur à 50 caractères, le nom retournera une chaine vide, sinon on met le nom en majuscule.
+                return nom.Length > 50 ? string.Empty : nom.ToUpper();
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        /// <summary>
+        ///     Gets or sets the prenom.
+        /// </summary>
+        public string Prenom
+        {
+            get
+            {
+                // Si la taille du prénom est supérieur à 50 caractères, le prénom retournera une chaine vide, sinon on met la première lettre du prénom en majuscule.
+                return prenom.Length > 50
+                           ? string.Empty
+                           : prenom[0].ToString().ToUpper() + prenom.Substring(1);
+            }
+
+            set
+            {
+                prenom = value;
+            }
+        }
+
+        /// <summary>
+        ///     Gets or sets the telephone.
+        /// </summary>
+        public string Telephone { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the type contact.
+        /// </summary>
+        public string TypeContact { get; set; }
+
+        /// <summary>
+        ///     Permet de récupérer la fonction "Infos" dans les autres classes.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="string" />.
+        /// </returns>
+        public abstract string Affiche();
+
+        /// <summary>
+        ///     affiche toutes les infos de base du contact
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="string" />.
+        /// </returns>
+        protected string Infos()
+        {
+            var ch = string.Empty;
+            ch +=
+                $"ID : {Id} \n Type de Contact : {TypeContact} \n Nom : {Nom} \n Prénom: {Prenom} \n Téléphone : {Telephone} \n";
+            return ch;
+        }
+    }
+}
---
+++ DllLibrary/ContactPro.cs
@@ -0,0 +1,76 @@
+﻿namespace DllLibrary
+{
+    /// <summary>
+    ///     La classe ContactPro qui hérite de Contact.
+    /// </summary>
+    public class ContactPro : Contact
+    {
+        /// <summary>
+        ///     Le string qui récupère le nom de l'entreprise.
+        /// </summary>
+        private string nomEntreprise;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ContactPro" /> class.
+        /// </summary>
+        /// <param name="id">
+        ///     The id.
+        /// </param>
+        /// <param name="c">
+        ///     The c.
+        /// </param>
+        /// <param name="n">
+        ///     The n.
+        /// </param>
+        /// <param name="p">
+        ///     The p.
+        /// </param>
+        /// <param name="t">
+        ///     The t.
+        /// </param>
+        /// <param name="a">
+        ///     The a.
+        /// </param>
+        /// <param name="ent">
+        ///     The ent.
+        /// </param>
+        public ContactPro(int id, string c, string n, string p, string t, string a, string ent)
+            : base(id, n, p, t, c)
+        {
+            AdresseMail = a;
+            NomEntreprise = ent;
+        }
+
+        /// <summary>
+        ///     Gets or sets the adresse mail.
+        /// </summary>
+        public string AdresseMail { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the nom entreprise.
+        /// </summary>
+        public string NomEntreprise
+        {
+            get
+            {
+                return nomEntreprise.Length > 50 ? string.Empty : nomEntreprise.ToUpper();
+            }
+
+            set
+            {
+                nomEntreprise = value;
+            }
+        }
+
+        /// <summary>
+        ///     Affiche les infos de Contact + les infos ContactsPro.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="string" />.
+        /// </returns>
+        public override string Affiche()
+        {
+            return Infos() + $" Adresse email : {AdresseMail} \n Nom de l'entreprise : {NomEntreprise}";
+        }
+    }
+}
